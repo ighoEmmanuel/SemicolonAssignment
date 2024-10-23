cc77c2fad4b04d7decfcc66d650d5db4cf3a5516
ighoEmmanuel <ighoe571@gmail.com>
2024-10-23 20:39:20 +0100

    SemicolonAssignment

---
+++ Phyton class/chaper 2/arrangement.py
@@ -0,0 +1,27 @@
+number1 = float(input("Enter first number: "))
+largest = number1
+smallest = number1
+central = number1
+number2 = float (input ("Enter second Number: "))
+if number2 > largest:
+    if largest == number2:
+      if number2  < smallest:
+        smallest == number2
+number3 = float(input("Enter third number: "))
+if number3 > largest:
+  largest = number3
+if number3 < smallest:
+        smallest == number3
+if largest == number1 and  smallest == number2 :
+      central = number3
+largest == number1 and smallest == number3
+central = number2
+largest == number3 and smallest == number2
+central  = number1
+largest = number3 and smallest == number1
+central = number2
+largest == number2 and smallest == number1
+central= number3
+if largest == number2 and smallest == number3:
+  central= number1
+print (smallest, central, largest)
