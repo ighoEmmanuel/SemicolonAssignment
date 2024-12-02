897f91c3ffc3e929dd94f22d9d7568bcbfe6fe00
ighoEmmanuel <ighoe571@gmail.com>
2024-12-02 15:29:53 +0100

    pythonClass

--- pythonClass/phaseGateOne/ascendingOrder.py
+++ pythonClass/phaseGateOne/ascendingOrder.py
@@ -1,3 +1,23 @@
+#prompt user to enter number three times 
+#collect
+#store each of them as first_number, second_number,third_number
+#if first_number < second_number and second_number < third_number
+#display first_number,second_number,third_number
+#if first_number > second_number and first_number < third_number
+#display second_number,first_number,third_number
+#if first_number < second_number and first_number > third_number
+#display third_number,first_number,second_number
+#if first_number == second_number and first_number == third_number
+#display third_number,first_number,second_number
+#if first_number > second_number and second_number > third_number
+#display third_number,second_number,first_number
+
+
+
+
+
+
+
 first_number = int(input("Enter first number:"))
 second_number = int(input("Enter second number:"))
 third_number = int(input("Enter third number:"))
@@ -13,3 +33,6 @@ if first_number < second_number and first_number > third_number:
 
 if first_number == second_number and first_number == third_number:
 	print(f"{third_number},{first_number},{second_number}")
+
+if first_number > second_number and second_number > third_number:
+	print(f"{third_number},{second_number},{first_number}")
