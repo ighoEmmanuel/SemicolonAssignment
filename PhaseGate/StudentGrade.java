76c922dd24535019a5a82805b76eb10c9a48aa5d
ighoEmmanuel <ighoe571@gmail.com>
2024-12-05 05:42:04 +0100

    SemicolonAssignment

--- PhaseGate/StudentGrade.java
+++ PhaseGate/StudentGrade.java
@@ -21,9 +21,13 @@ public static void score(int numbers, int subject){
 	for(int counter = 0 ; counter < subject ; counter++){
 		System.out.println("Enter Score for subject " + (counter + 1));
 		int score = scanner.nextInt();
+		System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println("Saved successfully");
 		while (score > 100 || score < 0){
 			System.out.println("Invalid Input please enter number between(0 - 100)");
 			score = scanner.nextInt();
+			System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>");
+			System.out.println("Saved successfully");
 }
 	scores[count][counter] = score;
 			}
@@ -34,6 +38,7 @@ public static void score(int numbers, int subject){
 		System.out.print("\t\tSUB " + (count + 1));
 }
 	System.out.print("\t\tTOTAL\t\tAVE\t\tPOS");
+	display();
 	for (int count = 0; count < numbers;count++){
 		int total = 0;
 		System.out.print("\nStudent " + (count + 1) + ":\t\t");
@@ -55,20 +60,41 @@ public static void score(int numbers, int subject){
 			position++;
 	}
 	}			
-		System.out.println("\t\t" + position);
+		System.out.print("\t\t" + position);
 	
 }
 
-		display();
+
+	display();
 for(int counter = 0; counter < subject;counter++){
 		System.out.print("\nSUBJECT SUMMARY");
 		System.out.print("\nSub " + (counter + 1));
+		int sum = 0;
+		int largest = 0;
+		int student = 0;
+		int smallest = scores[0][counter];
+		int smallestSubject = 0;
+		for(int count = 0; count < scores.length ; count++){
+			if (scores[count][counter] > largest){
+			largest = scores[count][counter];
+			student = count;
+}
+			if(scores[count][counter] < smallest){
+			smallest = scores[count][counter];
+			smallestSubject = count;
+}
+			sum += scores[count][counter];
+}
+		System.out.print("\nThe highest score is:" + "student " + (student + 1) + " scoring " + largest);
+		System.out.print("\nThe Smallest is:" + "student " +(smallestSubject + 1) + " scoring " + smallest);
+		double average = sum / numbers;
+		System.out.print("\nTotal:" + sum);
+		System.out.print("\nAverage score is:" + average);
+
 }
 }
-
-
 public static void display(){
-	System.out.println("==============================================================================================================================================================================================================================================================");
+	System.out.print("\n==============================================================================================================================================================================\n");
 
 }		
 
