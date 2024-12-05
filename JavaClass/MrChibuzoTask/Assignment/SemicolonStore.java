34429467d3e723c11bdb4f50d7e4ee84a9a6fa25
ighoEmmanuel <ighoe571@gmail.com>
2024-12-05 07:42:29 +0100

    SemicolonAssignment

--- JavaClass/MrChibuzoTask/Assignment/SemicolonStore.java
+++ JavaClass/MrChibuzoTask/Assignment/SemicolonStore.java
@@ -1,69 +1,114 @@
 import java.util.Scanner;
-
+import java.time.LocalDateTime;
 public class SemicolonStore {
 
     public static void main(String[] args) {
-        printItems();
+        printReceipt();
     }
 
-    public static String nameOfUser() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("What is the customer's Name?");
-        return s.nextLine();
-    }
-
-    public static String customerDetails() {
+    public static void printReceipt() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What did the customer buy? ");
-        return scanner.nextLine();
-    }
 
-    public static int numberOfPieces() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("How many pieces? ");
-        return scanner.nextInt();
-    }
+        System.out.println("Enter the customer's name: ");
+        String customerName = scanner.nextLine();
 
-    public static double perUnit() {
-        Scanner scanner = new Scanner(System.in);
+        String receipt = "";
+        double grandTotal = 0.0;
+        String addMore;
+
+        System.out.println("What did the customer buy? ");
+        String itemName = scanner.nextLine();
+
+        System.out.println("How many pieces? ");
+        int quantity = scanner.nextInt();
+
         System.out.print("How much per unit? ");
-        return scanner.nextDouble();
-    }
+        double unitPrice = scanner.nextDouble();
 
-    public static double total(int pieces, double unitPrice) {
-        return pieces * unitPrice;
-    }
-    public static StringBuilder options() {
-        Scanner s = new Scanner(System.in);
-        StringBuilder items = new StringBuilder();
-        String option;
+        double totalPrice = quantity * unitPrice;
+        grandTotal += totalPrice;
 
-        do {
-            String customerItem = customerDetails();
-            int pieces = numberOfPieces();
-            double unitPrice = perUnit();
-            double totalPrice = total(pieces, unitPrice);
+        receipt += itemName + "\t\t" + quantity + "\t\t" + unitPrice + "\t\t" + totalPrice + "\n";
 
-            items.append(String.format("\t\t%-20s\t%-10d\t%-10.2f\t%-10.2f\n", customerItem, pieces, unitPrice, totalPrice));
+        scanner.nextLine();
 
-            System.out.print("Add more Items? (yes/no): ");
-            option = s.next();
-        } while (option.equalsIgnoreCase("yes"));
+        System.out.println("Add more items? (yes/no): ");
+        addMore = scanner.nextLine();
 
-        return items;
-    }
+        while (addMore.equalsIgnoreCase("yes")) {
+            System.out.println("What did the customer buy? ");
+            itemName = scanner.nextLine();
 
+            System.out.println("How many pieces? ");
+            quantity = scanner.nextInt();
 
-    public static void printItems() {
-        Scanner scanner = new Scanner(System.in);
+            System.out.print("How much per unit? ");
+            unitPrice = scanner.nextDouble();
 
-        String customerName = nameOfUser();
-        StringBuilder allItems = new StringBuilder();
-        allItems.append("\n\t\tITEM\t\tQTY\t\tPRICE\t\t\tTOTAL(NGN)\n");
-        allItems.append("----------------------------------------------------------------\n");
+            totalPrice = quantity * unitPrice;
+            grandTotal += totalPrice;
 
-        allItems.append(options());
+            receipt += itemName + "\t\t" + quantity + "\t\t" + unitPrice + "\t\t" + totalPrice + "\n";
 
-        System.out.println(allItems.toString());
+            scanner.nextLine();
+
+            System.out.println("Add more items? (yes/no): ");
+            addMore = scanner.nextLine();
+        }
+
+        System.out.println("What is your name?");
+        String cashierName = scanner.nextLine();
+
+        System.out.println("How much discount will the customer get?");
+        double discount = scanner.nextDouble();
+	LocalDateTime date = LocalDateTime.now();
+	double vatRate = 0.1750;
+	 double vateTotal = grandTotal * vatRate;
+	double billTotal = (grandTotal - discount) * vatRate;
+	System.out.println("SEMICOLON STORES");
+	System.out.println("MAIN BRANCH");
+	System.out.println("LOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.");
+	System.out.println("TEL: 83293828343");
+	System.out.println("Date: " + date);
+	System.out.println("Cashier: " + cashierName);
+	System.out.println("Customer Name: " + customerName);
+        System.out.println("\nITEM\t\tQTY\t\tPRICE\t\tTOTAL(NGN)");
+        System.out.println("============================================================");
+        System.out.println(receipt);
+        System.out.println("------------------------------------------------------------");
+        System.out.println("\t\tGrand Total: " + "\t\t\t" + grandTotal);
+        System.out.println("\t\tDiscount: " + "\t\t\t" + discount);
+	System.out.println("\t\tVAT @ 17.50%:" + "\t\t\t" + vateTotal);
+        System.out.println("============================================================");
+	System.out.println("\t\tBill Total:" + "\t\t\t" + billTotal);
+        System.out.println("============================================================");
+	System.out.println("THIS IS NOT AN RECEIPT KINDLY PAY " + billTotal);
+	
+	System.out.println("How much did the customer give to you?");
+	double paided = scanner.nextDouble();
+	
+	double balance = paided - billTotal;
+	System.out.println("SEMICOLON STORES");
+	System.out.println("MAIN BRANCH");
+	System.out.println("LOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.");
+	System.out.println("TEL: 83293828343");
+	System.out.println("Date: " + date);
+	System.out.println("Cashier: " + cashierName);
+	System.out.println("Customer Name: " + customerName);
+        System.out.println("\nITEM\t\tQTY\t\tPRICE\t\tTOTAL(NGN)");
+        System.out.println("============================================================");
+        System.out.println(receipt);
+        System.out.println("------------------------------------------------------------");
+        System.out.println("\t\tGrand Total: " + "\t\t\t" + grandTotal);
+        System.out.println("\t\tDiscount: " + "\t\t\t" + discount);
+	System.out.println("\t\tVAT @ 17.50%:" + "\t\t\t" + vateTotal);
+        System.out.println("============================================================");
+	System.out.println("\t\tBill Total:" + "\t\t\t" + billTotal);
+	System.out.println("\t\tAmount paid:" + "\t\t\t" + paided);
+	System.out.println("\t\tBalance:" + "\t\t\t" + balance);
+        System.out.println("============================================================");
+	System.out.println("THANK YOU FOR YOUR PATRONAGE");
+        System.out.println("============================================================");
+
     }
 }
