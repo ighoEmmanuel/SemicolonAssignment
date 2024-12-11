35b8ba8e3bc287d30f73cfed7a2482d0234a1d19
ighoEmmanuel <ighoe571@gmail.com>
2024-12-11 21:33:24 +0100

    SemiColonAssignment

--- BreakAssignment/TicTacToe.java
+++ BreakAssignment/TicTacToe.java
@@ -4,7 +4,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[][] board = new String[3][3];
-        
+
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 board[row][col] = "EMPTY";
@@ -12,39 +12,48 @@ public class TicTacToe {
             }
             System.out.println();
         }
-	for (int move = 0; move < 9; move++){
-        for (int count = 0; count < 2; count++) {
+
+        for (int move = 0; move < 9; move++) {
+            int count = move % 2;
             System.out.println("Player " + (count + 1));
 
-            System.out.println("Enter number of row:");
-            int row = scanner.nextInt();
-            while (row > 3 || row < 1) {
-                System.out.println("Invalid input. Row must be between 1 and 3.");
+            int row = -1;
+            int col = -1;
+
+            int inputAttempts = 0;
+            do {
+                if (inputAttempts > 0) {
+                    System.out.println("Invalid input or cell already occupied. Try again.");
+                }
+                System.out.println("Enter number of row (1-3):");
                 row = scanner.nextInt();
-            }
-
-            System.out.println("Enter number of column:");
-            int col = scanner.nextInt();
-            while (col > 3 || col < 1) {
-                System.out.println("Invalid input. Column must be between 1 and 3.");
+                System.out.println("Enter number of column (1-3):");
                 col = scanner.nextInt();
-            }
+                inputAttempts++;
+            } while (row < 1 || row > 3 || col < 1 || col > 3 || !board[row - 1][col - 1].equals("EMPTY"));
 
+        
             board[row - 1][col - 1] = count == 0 ? "X" : "O";
-
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     System.out.print(board[i][j] + " ");
                 }
                 System.out.println();
             }
+
+            if ((board[0][0].equals(board[0][1]) && board[0][1].equals(board[0][2]) && !board[0][0].equals("EMPTY")) ||
+                (board[1][0].equals(board[1][1]) && board[1][1].equals(board[1][2]) && !board[1][0].equals("EMPTY")) ||
+                (board[2][0].equals(board[2][1]) && board[2][1].equals(board[2][2]) && !board[2][0].equals("EMPTY")) ||
+                (board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0]) && !board[0][0].equals("EMPTY")) ||
+                (board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1]) && !board[0][1].equals("EMPTY")) ||
+                (board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2]) && !board[0][2].equals("EMPTY")) ||
+                (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals("EMPTY")) ||
+                (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals("EMPTY"))) {
+                System.out.println("Player " + (count + 1) + " Wins!");
+                return;
+            }
         }
-	if(board[0][0].equals("X")&&board[0][1].equals("X")&&board[0][2].equals("X")){
-	System.out.println();
-	System.out.print("Player 1 Wins");
-		break;
-}
-		
-}
+
+        System.out.println("It's a draw!");
     }
 }
