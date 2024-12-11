eafad2323d14a5c8dbbcecd2c6dd9f9466719fb2
ighoEmmanuel <ighoe571@gmail.com>
2024-12-11 16:05:44 +0100

    SemiColonAssignment

--- BreakAssignment/TicTacToe.java
+++ BreakAssignment/TicTacToe.java
@@ -12,7 +12,7 @@ public class TicTacToe {
             }
             System.out.println();
         }
-
+	for (int move = 0; move < 9; move++){
         for (int count = 0; count < 2; count++) {
             System.out.println("Player " + (count + 1));
 
@@ -39,5 +39,12 @@ public class TicTacToe {
                 System.out.println();
             }
         }
+	if(board[0][0].equals("X")&&board[0][1].equals("X")&&board[0][2].equals("X")){
+	System.out.println();
+	System.out.print("Player 1 Wins");
+		break;
+}
+		
+}
     }
 }
